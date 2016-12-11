748166d0259ac35f209382f5ab19af0dafc4aa97
iceStone <ice@micua.com>
2016-12-11 22:28:28 +0800

    nprogress with ajax request

---
+++ app/libraries/resource.js
@@ -0,0 +1,14 @@
+import Vue from 'vue'
+import Resource from 'vue-resource'
+import NProgress from 'nprogress'
+
+// https://github.com/pagekit/vue-resource/blob/master/docs/config.md
+// Vue.http.headers.common['Authorization'] = 'Basic YXBpOnBhc3N3b3Jk'
+
+Vue.use(Resource)
+
+// Progress with AJAX
+Vue.http.interceptors.push((request, next) => {
+  request.before = () => NProgress.start()
+  next(response => { NProgress.done() })
+})
--- app/libraries/router.js
+++ app/libraries/router.js
@@ -1,13 +1,20 @@
 import Vue from 'vue'
 import Router from 'vue-router'
+import NProgress from 'nprogress'
 
 import routes from './routes'
 
 Vue.use(Router)
 
 // http://router.vuejs.org/en/api/options.html
-export default new Router({
+const router = new Router({
   mode: 'history',
   base: '/',
   routes: routes
 })
+
+// Loading progress
+router.beforeEach((to, from, next) => NProgress.start() && next())
+router.afterEach(route => NProgress.done())
+
+export default router
