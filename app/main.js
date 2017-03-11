12a885a6224c30165312bcfb1069e3d2ff67dd47
iceStone <ice@micua.com>
2017-03-12 00:36:34 +0800

    Initial commit

--- app/main.js
+++ app/main.js
@@ -9,7 +9,7 @@ import progressBar from 'libraries/plugins/progress-bar'
 
 // ## Import styles
 // =========================
-import 'assets/styles/element.css'
+import 'assets/element/index.css'
 
 // ## Use plugins
 // =========================
