af68aab6d7bbc7657371f7bf71b249e55ec546cb
iceStone <ice@micua.com>
2017-03-12 22:46:38 +0800

    Initial commit

--- app/views/login.vue
+++ app/views/login.vue
@@ -3,14 +3,9 @@
     <section class="login">
       <header class="login-header">
         <h1 class="brand"><a href="#" tabindex="-1">WEDN.NET</a></h1>
-        <div class="alert alert-danger" role="alert">
-          <strong>错误：</strong>
-          用户名或密码错误！
-          <a class="alert-link" href="#">忘记密码？</a>
-          <button type="button" class="close" aria-label="Close"><span aria-hidden="true">&times;</span></button>
-        </div>
+        <el-alert :title="error" :description="message" v-show="error" class="alert" type="warning" show-icon/>
       </header>
-      <form class="login-form">
+      <!-- <form class="login-form">
         <h2 class="heading">登录</h2>
         <div class="form-group">
           <label for="username">用户名</label>
@@ -21,7 +16,17 @@
           <input type="password" id="password" class="form-control" placeholder="密码">
         </div>
         <button class="btn btn-primary btn-block">登录</button>
-      </form>
+      </form> -->
+      <el-form :model="model" :rules="rules" class="login-form" ref="login-form" auto-complete="off" label-position="top">
+        <h2 class="heading">登录</h2>
+        <el-form-item label="用户名" prop="username">
+          <el-input type="text" v-model="model.username" placeholder="请输入用户名"/>
+        </el-form-item>
+        <el-form-item label="密码" prop="password">
+          <el-input type="password" v-model="model.password" placeholder="请输入密码"/>
+        </el-form-item>
+        <el-button type="primary" native-type="submit" @click.prevent="submit">提交</el-button>
+      </el-form>
       <footer class="login-footer">
         ← 返回到 <a href="/">WEDN.NET</a>
       </footer>
@@ -31,6 +36,73 @@
 
 <script>
   export default {
-    name: 'login'
+    name: 'login',
+    data () {
+      return {
+        error: '',
+        message: '',
+        model: {
+          username: '',
+          password: ''
+        },
+        rules: {
+          username: [
+            { required: true, message: '请输入用户名' },
+            { min: 2, max: 16, message: '长度在 2 到 16 个字符' }
+          ],
+          password: [
+            { required: true, message: '请输入密码' },
+            { min: 6, max: 16, message: '长度在 6 到 16 个字符' }
+          ]
+        }
+      }
+    },
+    // beforeCreate () {
+    //   if (store.getters.token) {
+    //     this.$router.replace({ path: this.$route.query.redirect || '/' })
+    //   }
+    // },
+    methods: {
+      submit () {
+        this.error = ''
+        this.$refs['login-form'].validate(valid => {
+          if (!valid) return false
+          this.$store.dispatch('createToken', {
+            username: this.model.username,
+            password: this.model.password
+          })
+          .then(token => {
+            this.$router.replace({ path: this.$route.query.redirect || '/' })
+          })
+          .catch(err => {
+            this.error = '错误'
+            if (err.data && err.data.error) {
+              switch (err.status) {
+                case 401:
+                  this.message = '用户名或密码错误！'
+                  break
+                case 500:
+                  this.message = '服务器内部异常，请稍后再试！'
+                  break
+                default:
+                  this.message = '出现异常，请稍后再试！'
+              }
+            } else {
+              this.message = err.message
+            }
+          })
+        })
+      }
+    }
   }
 </script>
+
+<style scoped>
+  .el-form-item {
+    margin-bottom: 30px
+  }
+
+  .el-button {
+    width: 100%;
+  }
+</style>
--- scripts/deploy.js
+++ scripts/deploy.js
@@ -11,13 +11,14 @@ const check = `cd ${config.paths.output} && git status -s`
 const commit = `cd ${config.paths.output} && git add --all && git commit -m '${message}' && git push -u origin gh-pages`
 
 exec(check, (err, stdout, stderr) => {
+  if (err) console.error(err)
   if (!stdout.toString().trim()) {
     console.log(chalk.yellow(`\n  No change in ${config.paths.output}\n`))
     return spinner.stop()
   }
   exec(commit, (err, stdout, stderr) => {
     if (err) console.error(err)
-    console.log(chalk.green(message))
+    console.log(chalk.green(`\n  ${message}\n`))
     spinner.stop()
   })
 })
