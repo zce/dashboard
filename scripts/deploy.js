6ef158687040b1f5516b5f54ac8c77ba4a9309c1
iceStone <ice@micua.com>
2017-03-12 22:07:48 +0800

    Initial commit

--- scripts/deploy.js
+++ scripts/deploy.js
@@ -1,13 +1,23 @@
 const { exec } = require('child_process')
 const ora = require('ora')
+const chalk = require('chalk')
 const config = require('../config')
 
 const spinner = ora('Deploy to gh-pages...')
 spinner.start()
 
-const cmd = `cd ${config.paths.output} && git add --all && git commit -m 'Update @ ${new Date()}' && git push -u origin gh-pages`
+const message = `Update @ ${new Date()}`
+const check = `cd ${config.paths.output} && git status -s`
+const commit = `cd ${config.paths.output} && git add --all && git commit -m '${message}' && git push -u origin gh-pages`
 
-exec(cmd, (err, stdout, stderr) => {
-  if (err) console.error(err)
-  spinner.stop()
+exec(check, (err, stdout, stderr) => {
+  if (!stdout.toString().trim()) {
+    console.log(chalk.yellow(`\n  No change in ${config.paths.output}\n`))
+    return spinner.stop()
+  }
+  exec(commit, (err, stdout, stderr) => {
+    if (err) console.error(err)
+    console.log(chalk.green(message))
+    spinner.stop()
+  })
 })
