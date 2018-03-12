30d0784c2f48faf322782193612e0e071c93e3a8
zce <w@zce.me>
2018-03-12 10:35:49 +0800

    :boom: Created by vue-cli 3.x

--- src/views/main/about.vue
+++ src/views/main/about.vue
@@ -1,10 +1,10 @@
 <template>
   <div>
     <div class="heading">
-      <h1 class="title">关于WEDN.NET</h1>
+      <h1 class="title">关于 WEDN.NET</h1>
     </div>
-    <p><strong>关于WEDN.NET：</strong> <br> 咱们等着瞧！</p>
-    <p><strong>关于我：</strong> <br> 我是汪磊！</p>
+    <p><strong>关于 WEDN.NET：</strong> <br> 咱们等着瞧！</p>
+    <p><strong>关于我：</strong> <br> 我是汪磊！ <br> <a href="https://zce.me">https://zce.me</a> <br> <a href="https://github.com/zce">https://github.com/zce</a></p>
   </div>
 </template>
 
