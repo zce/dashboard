ed3d59b79979d76c4889c76d7e0336b444a78bc0
zce <w@zce.me>
2019-12-14 13:27:33 +0800

    fix: update deps

--- src/i18n/index.js
+++ src/i18n/index.js
@@ -7,8 +7,8 @@ Vue.use(I18n)
 export default new I18n({
   locale: 'en',
   messages: {
-    'en': require('./locales/en'),
-    'cn': require('./locales/cn'),
-    'ja': require('./locales/ja')
+    en: require('./locales/en'),
+    cn: require('./locales/cn'),
+    ja: require('./locales/ja')
   }
 })
